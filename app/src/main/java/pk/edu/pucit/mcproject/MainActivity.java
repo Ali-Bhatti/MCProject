731b2e4f6379304bf0da5cee70f95e6d74a242db
Ali-Bhatti <m.alibhatti55@gmail.com>
2020-05-26 12:43:26 +0500

    Search Icon in toolbar added

--- app/src/main/java/pk/edu/pucit/mcproject/MainActivity.java
+++ app/src/main/java/pk/edu/pucit/mcproject/MainActivity.java
@@ -1,14 +1,50 @@
 package pk.edu.pucit.mcproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.list);
+        String [] items = {"Bagshahi Mosque","Minar-e-Pakistan","Shahi Qala","Behria Grand Mosque","Anarkli","Masjid wazir khan","Mochi gate"};
+        arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1 , items);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search_icon);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Search Place here");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                arrayAdapter.getFilter().filter(newText);
+                return true ;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+
     }
 }

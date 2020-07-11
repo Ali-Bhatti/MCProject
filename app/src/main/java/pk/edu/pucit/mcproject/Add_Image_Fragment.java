21a6511165904658834acd6874adf831054663a4
usman <bsef17m028@pucit.edu.pk>
2020-07-11 11:27:09 +0500

    completed the functionality of choosing image from gallery

---
+++ app/src/main/java/pk/edu/pucit/mcproject/Add_Image_Fragment.java
@@ -0,0 +1,125 @@
+package pk.edu.pucit.mcproject;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import static android.app.Activity.RESULT_OK;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Add_Image_Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Add_Image_Fragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Button mButtonChooseImage;
+    private Button mButtonUpload;
+    private TextView mTextViewShowUploads;
+    private EditText PlaceName;
+    private ImageView mImageView;
+    private ProgressBar mProgressBar;
+    private Uri Image_uri;
+
+    public Add_Image_Fragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Add_Image_Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Add_Image_Fragment newInstance(String param1, String param2) {
+        Add_Image_Fragment fragment = new Add_Image_Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_add__image_, container, false);
+
+        mButtonChooseImage = view.findViewById(R.id.btn_Browse_Image);
+        mButtonUpload = view.findViewById(R.id.btn_Upload);
+        //PlaceName = view.findViewById(R.id.edit_text_file_name);
+        mImageView = view.findViewById(R.id.imageView);
+        mProgressBar = view.findViewById(R.id.progressBar);
+
+
+        mButtonChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
+
+
+        mButtonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        return view;
+    }
+
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            Image_uri = data.getData();
+            Picasso.get().load(Image_uri).into(mImageView);
+        }
+    }
+}
